1ff0de38e656889f793074b104f37bffa96122f1
himanshu <himanshuiitjee1@gmail.com>
2024-07-03 09:55:55 +0530

    last change

---
+++ index.js
@@ -0,0 +1,71 @@
+import express from "express";
+import cors from "cors";
+import { StreamChat } from "stream-chat";
+import { v4 as uuidv4 } from "uuid";
+import bcrypt from "bcrypt";
+const app = express();
+
+const corsOptions = {
+  origin: 'http://localhost:3000', // Replace with your frontend URL
+  methods: ['GET', 'POST'], // Add more methods as needed
+  allowedHeaders: ['Content-Type', 'Authorization'], // Add more headers as needed
+};
+
+// app.use(cors(corsOptions));
+
+app.use(express.static('build'));
+
+app.use(express.json());
+const api_key = "7te7jj26v8wg";
+const api_secret =
+  "3s6ajfjt48c64xbhguuahku2nvpqqavaqg2sbvshytk2rnpd4f96c82hacby8x5z";
+const serverClient = StreamChat.getInstance(api_key, api_secret);
+
+app.post("/signup", async (req, res) => {
+  try {
+    const { firstName, lastName, username, password } = req.body;
+    const userId = uuidv4();
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const token = serverClient.createToken(userId);
+    res.json({ token, userId, firstName, lastName, username, hashedPassword });
+  } catch (error) {
+    res.json(error);
+  }
+});
+
+app.post("/login", async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const { users } = await serverClient.queryUsers({ name: username });
+    if (users.length === 0) return res.json({ message: "User not found" });
+
+    const token = serverClient.createToken(users[0].id);
+    const passwordMatch = await bcrypt.compare(
+      password,
+      users[0].hashedPassword
+    );
+
+    if (passwordMatch) {
+      res.json({
+        token,
+        firstName: users[0].firstName,
+        lastName: users[0].lastName,
+        username,
+        userId: users[0].id,
+      });
+    }
+  } catch (error) {
+    res.json(error);
+  }
+});
+
+app.listen(3001, () => {
+  console.log("Server is running on port 3001");
+});
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://multiplayer-xno-front.vercel.app');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
